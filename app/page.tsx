11e5d010be6476510e3b95a3f1009f8d8a582734
ryotarofr <ryoryo.fr0608@icloud.com>
2023-12-26 14:43:38 +0900

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CarouselDemo from '@/components/Crousel/carousel'
 import ToggleThemeButton from '@/components/ui/toggle-theme'
 
 export default function Home() {
   return (
     <div>
       <ToggleThemeButton />
+      <CarouselDemo />
     </div>
   )
 }
